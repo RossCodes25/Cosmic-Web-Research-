f8ee29416e38d337974d0ee4d13e893635ba7cd7
RossMcPhee23 <rossmcphee82@gmail.com>
2025-06-26 15:40:04 +0100

    problem import, not fixed

--- data_storage.py
+++ data_storage.py
@@ -2,19 +2,21 @@ import numpy as np
 
 data_dir = '/Users/rossmcphee/Desktop/CAMELS/CMD/CMD/Data/'
 
-# First read the parameter file, which lists the specific values of each of the 28 parameters that are varied in the 1P set, for each set of 3D simulations. 
-# name of the parameter file
-fparams = data_dir + 'params_1P_IllustrisTNG.txt'
-# read the data
-params = np.loadtxt(fparams)
+import numpy as np
 
-#Name of the file with the 2D maps - it stores 2100 maps of HI = 28 parameters x 5 values in a  x 15 slices
-fnameHI=data_dir+'Maps_HI_IllustrisTNG_1P_z=0.00.npy'
-fnameGAS = data_dir+'Maps_Mgas_IllustrisTNG_1P_z=0.00.npy'
-#read the data
-maps_1p_TNG_HI = np.load(fnameHI)
-maps_1p_TNG_MGAS = np.load(fnameGAS)
-print(np.shape(maps_1p_TNG_HI)) # this tells us it is a 256x256 2D grid 
-print(np.shape(maps_1p_TNG_MGAS))
+data_dir = '/Users/rossmcphee/Desktop/CAMELS/CMD/CMD/Data/'
+
+def load_params():
+    return np.loadtxt(data_dir + 'params_1P_IllustrisTNG.txt')
+
+def load_maps_hi():
+    return np.load(data_dir + 'Maps_HI_IllustrisTNG_1P_z=0.00.npy')
+
+def load_maps_mgas():
+    return np.load(data_dir + 'Maps_Mgas_IllustrisTNG_1P_z=0.00.npy')
+
+
+#print(np.shape(maps_1p_TNG_HI)) # this tells us it is a 256x256 2D grid 
+#print(np.shape(maps_1p_TNG_MGAS))
 
 # New addition of M gas to this also 

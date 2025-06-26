a2982771c673dfc3a72d591ab6dd5c10ed3fbfb9
RossMcPhee23 <rossmcphee82@gmail.com>
2025-06-26 16:52:41 +0100

    further clarity: MGas

--- data_storage.py
+++ data_storage.py
@@ -15,6 +15,9 @@ def load_maps_hi():
 def load_maps_mgas():
     return np.load(data_dir + 'Maps_Mgas_IllustrisTNG_1P_z=0.00.npy')
 
+def load_maps_mcdm():
+    return np.load(data_dir + 'Maps_Mcdm_IllustrisTNG_1P_z=0.00.npy')
+
 
 #print(np.shape(maps_1p_TNG_HI)) # this tells us it is a 256x256 2D grid 
 #print(np.shape(maps_1p_TNG_MGAS))
